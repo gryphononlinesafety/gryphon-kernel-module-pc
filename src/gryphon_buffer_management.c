c1b14c47ba197142f607db9d5028b250b32e6bbf
Naveen Kumar Gutti <naveenkumar.gvvsnr@gmail.com>
2025-01-30 02:34:28 +0530

    GryphonConnect Copyright added

--- src/gryphon_buffer_management.c
+++ src/gryphon_buffer_management.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2025 Naveen Kumar Gutti <naveen@gryphonconnect.com> GryphonConnect
+ * Copyright (C) 2025 GryphonConnect [ https://gryphonconnect.com/ ]
+ * Author: Naveen Kumar Gutti <naveen@gryphonconnect.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 2.1
