13615521d21bb2cd199b4344f25bf45c28bb1c63
Naveen Kumar. G <naveen@gryphonconnect.com>
2026-08-04 10:38:40 +0530

    skb marked packets with 0x9 will be allowed directly

--- src/gryphon_dpi.c
+++ src/gryphon_dpi.c
@@ -2730,6 +2730,9 @@ static unsigned int gry_prerouting_packet_process_hook(void *priv, struct sk_buf
 		return NF_ACCEPT;
 	}
 
+	if (skb->mark == 0x9)
+		return NF_ACCEPT;
+
 	// check the interface to listen
 	if((skb->dev == NULL) || (strncmp(skb->dev->name, "br-", 3) != 0)){
 #ifdef ENABLE_GRY_MARK
