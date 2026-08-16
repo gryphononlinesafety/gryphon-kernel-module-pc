ed1442ab761a92002dfc1b12cd56c6d4ace9d8f4
Naveen Kumar. G <naveen@gryphonconnect.com>
2026-08-16 18:39:21 +0530

    Gryphon Kernel Module version 01.0001.19

--- src/gryphon_dpi.c
+++ src/gryphon_dpi.c
@@ -41,7 +41,7 @@
 #include <net/netfilter/nf_conntrack_core.h>
 #include "gryphon_buffer_management.h"
 
-#define GRY_MODULE_VERSION "01.0001.18"
+#define GRY_MODULE_VERSION "01.0001.19"
 
 #define PORTSCAN_ENABLED 1
 #define GRYPHON_DEBUG_ENABLED 0
